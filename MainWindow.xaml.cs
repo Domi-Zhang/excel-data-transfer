e6012d90e864989905399629cf14af2306e021d4
Domi-Zhang <znfsky@hotmail.com>
2014-02-20 20:50:24 +0800

    none

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,11 +26,39 @@ namespace excel_data_transfer
         public MainWindow()
         {
             InitializeComponent();
+
+            List<ColumnMapping> mappingConfigs=new List<ColumnMapping>();
+
+            string[] configs=File.ReadAllLines("mapping.txt");
+            for (int i = 0; i < configs.Length; i++)
+			{
+                string[] config=configs[i].Split(' ');
+			    ColumnMapping columnMapping=new ColumnMapping(){ IsPrimaryKey=i==0, SourceName=config[0], TargetName=config[1]};
+                mappingConfigs.Add(columnMapping);
+            }
+            
+
+            GridView gvMapping = new GridView();
+            gvMapping.Columns.Add(new GridViewColumn() { DisplayMemberBinding = new Binding("SourceName"), Header = "原始列名" });
+            gvMapping.Columns.Add(new GridViewColumn() { DisplayMemberBinding = new Binding("TargetName"), Header = "目标列名" });
+
+            ListView lvMapping = new ListView();
+            lvMapping.ItemsSource = mappingConfigs;
+            lvMapping.View = gvMapping;
+
+            sp_columnMapping.Children.Add(lvMapping);
         }
 
         private void btn_addTgtFileName_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Excel files (*.xls,*.xlsx)|*.xls;*.xlsx";
 
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                sp_targetFileNames.Children.Add(new TextBlock() { Text = dlg.SafeFileName });
+            }
         }
 
         private void btn_addSrcFileName_Click(object sender, RoutedEventArgs e)
@@ -41,31 +69,33 @@ namespace excel_data_transfer
             Nullable<bool> result = dlg.ShowDialog();
             if (result == true)
             {
-                using (FileStream stream = new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
-                {
-                    IWorkbook workbook = new HSSFWorkbook(stream);
-                    ISheet hs = workbook.GetSheet(workbook.GetSheetName(0));
-
-                    IRow header = hs.GetRow(0);
-                    List<ICell> headerCells = header.Cells;
-                    List<ColumnMapping> mappingList = new List<ColumnMapping>();
-
-                    for (int i = 0; i < headerCells.Count; i++)
-                    {
-                        mappingList.Add(new ColumnMapping() { SourceIndex=i, SourceName=headerCells[i].ToString() });
-                    }
-
-                    GridView gvMapping = new GridView();
-                    gvMapping.Columns.Add(new GridViewColumn(){ DisplayMemberBinding=new Binding("SourceName"), Header="原始列名"});
-
-                    ListView lvMapping=new ListView();
-                    lvMapping.ItemsSource=mappingList;
-                    lvMapping.View=gvMapping;
-
-                    sp_columnMapping.Children.Add(lvMapping);
-                }
+                sp_sourceFileNames.Children.Add(new TextBlock() { Text = dlg.SafeFileName });
             }
         }
+
+        //using (FileStream stream = new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+        //        {
+        //            IWorkbook workbook = new HSSFWorkbook(stream);
+        //            ISheet hs = workbook.GetSheet(workbook.GetSheetName(0));
+
+        //            IRow header = hs.GetRow(0);
+        //            List<ICell> headerCells = header.Cells;
+        //            List<ColumnMapping> mappingList = new List<ColumnMapping>();
+
+        //            for (int i = 0; i < headerCells.Count; i++)
+        //            {
+        //                mappingList.Add(new ColumnMapping() { SourceIndex = i, SourceName = headerCells[i].ToString() });
+        //            }
+
+        //            GridView gvMapping = new GridView();
+        //            gvMapping.Columns.Add(new GridViewColumn() { DisplayMemberBinding = new Binding("SourceName"), Header = "原始列名" });
+
+        //            ListView lvMapping = new ListView();
+        //            lvMapping.ItemsSource = mappingList;
+        //            lvMapping.View = gvMapping;
+
+        //            sp_columnMapping.Children.Add(lvMapping);
+        //        }
 
     }
 
