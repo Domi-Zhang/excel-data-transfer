3c4e9779e8f786f77ab694b74fd6d2d1b49134b9
Domi-Zhang <znfsky@hotmail.com>
2014-02-22 09:24:32 +0800

    none

---
+++ ColumnMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace excel_data_transfer
+{
+    class ColumnMapping
+    {
+        public string SourceFile { get; set; }
+        public string SourceName { get; set; }
+        public string TargetName { get; set; }
+    }
+}
