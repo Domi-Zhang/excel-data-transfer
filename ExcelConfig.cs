4fa1845c72b15d199281151f8415124dba445118
Domi-Zhang <znfsky@hotmail.com>
2014-02-25 13:38:12 +0800

    none

---
+++ ExcelConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace excel_data_transfer
+{
+    class ExcelConfig
+    {
+        public string FileName { get; set; }
+        public int HeaderRow { get; set; }
+        public int SheetIndex { get; set; }
+    }
+}
